8c90d3e1deecd26f978931ac9645c69427b31722
tech-camp <tech-camp@tech-camp-1-073-no-MacBook-Air.local>
2017-04-03 18:10:05 +0900

    test

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -10,37 +10,3 @@ $(function(){
   });
 
 });
-
-
-<script>
-jQuery(function($){
-
-  var min_width = 768;
-  //画面幅による分岐と imagesLoaded, Masonry のイニシャライズを関数化
-  function masonry_update() {
-    var $container = $('#container');
-    if ( $('html').width() < min_width ) {
-      $container.masonry('destroy');
-    } else {
-        $container.masonry({
-          itemSelector: '.item',
-          columnWidth: 300,
-          gutter: 4
-        });
-    }
-  }
-
-  masonry_update();
-
-  //リサイズ時の処理
-  var timer = false;
-  $(window).resize(function(){
-    if (timer !== false) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(function() {
-      masonry_update();
-    }, 200);
-  });
-});
-</script>
